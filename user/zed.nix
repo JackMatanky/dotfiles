8c0b99d54c9651840b457b256b6b1e79e366514e
JackMatanky <JackMatanky@gmail.com>
2024-11-17 23:32:25 +0200

    try fixing ruff in zed

--- user/zed.nix
+++ user/zed.nix
@@ -24,14 +24,15 @@ in {
     file = {
       "${config}settings.json".source = ../.zed/settings.json;
       "${config}keymap.json".source = ../.zed/keymap.json;
-      "${snippets}${gitcommit-snip}".source = ../.zed/snippets/${gitcommit-snip};
-      "${snippets}${html-snip}".source = ../.zed/snippets/${html-snip};
-      "${snippets}${javascript-snip}".source = ../.zed/snippets/${javascript-snip};
-      "${snippets}${latex-snip}".source = ../.zed/snippets/${latex-snip};
-      "${snippets}${markdown-snip}".source = ../.zed/snippets/${markdown-snip};
-      "${snippets}${nix-snip}".source = ../.zed/snippets/${nix-snip};
-      "${snippets}${python-snip}".source = ../.zed/snippets/${python-snip};
-      "${snippets}${sql-snip}".source = ../.zed/snippets/${sql-snip};
+      "${snippets}".source = ../.zed/snippets;
+      # "${snippets}${gitcommit-snip}".source = ../.zed/snippets/${gitcommit-snip};
+      # "${snippets}${html-snip}".source = ../.zed/snippets/${html-snip};
+      # "${snippets}${javascript-snip}".source = ../.zed/snippets/${javascript-snip};
+      # "${snippets}${latex-snip}".source = ../.zed/snippets/${latex-snip};
+      # "${snippets}${markdown-snip}".source = ../.zed/snippets/${markdown-snip};
+      # "${snippets}${nix-snip}".source = ../.zed/snippets/${nix-snip};
+      # "${snippets}${python-snip}".source = ../.zed/snippets/${python-snip};
+      # "${snippets}${sql-snip}".source = ../.zed/snippets/${sql-snip};
     };
   };
 }
