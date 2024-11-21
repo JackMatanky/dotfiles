70536264ca844c5f69981542d40aa23828554d02
JackMatanky <JackMatanky@gmail.com>
2024-11-21 10:48:52 +0200

    changed .zed folder to zed

--- user/zed.nix
+++ user/zed.nix
@@ -5,7 +5,6 @@
   # userSettings,
   ...
 }: let
-  # Create the Python environment with Python 3.12 and necessary packages
   config = ".config/zed/";
   snippets = "${config}snippets/";
   gitcommit-snip = "gitcommit.json";
@@ -22,17 +21,9 @@ in {
       zed-editor # Zed IDE
     ];
     file = {
-      "${config}settings.json".source = ../.zed/settings.json;
-      "${config}keymap.json".source = ../.zed/keymap.json;
-      "${snippets}".source = ../.zed/snippets;
-      # "${snippets}${gitcommit-snip}".source = ../.zed/snippets/${gitcommit-snip};
-      # "${snippets}${html-snip}".source = ../.zed/snippets/${html-snip};
-      # "${snippets}${javascript-snip}".source = ../.zed/snippets/${javascript-snip};
-      # "${snippets}${latex-snip}".source = ../.zed/snippets/${latex-snip};
-      # "${snippets}${markdown-snip}".source = ../.zed/snippets/${markdown-snip};
-      # "${snippets}${nix-snip}".source = ../.zed/snippets/${nix-snip};
-      # "${snippets}${python-snip}".source = ../.zed/snippets/${python-snip};
-      # "${snippets}${sql-snip}".source = ../.zed/snippets/${sql-snip};
+      "${config}settings.json".source = ../../zed/settings.json;
+      "${config}keymap.json".source = ../../zed/keymap.json;
+      "${snippets}".source = ../../zed/snippets;
     };
   };
 }
