14f2d7fa12e97d6d61c520023045a573a391142b
JackMatanky <JackMatanky@gmail.com>
2024-11-22 13:18:28 +0200

    revert to previous locales and explicitly enable all locales

--- nixos/system/locale.nix
+++ nixos/system/locale.nix
@@ -11,28 +11,26 @@
   i18n = {
     glibcLocales = pkgs.glibcLocales.override {
       locales = [
-        "C.UTF-8"
-        # systemSettings.localeDefault
-        # systemSettings.localeSecondary
+        systemSettings.localeDefault
+        systemSettings.localeSecondary
       ];
-      allLocales = false; # Explicitly disable building all locales
+      allLocales = true; # Explicitly disable building all locales
     };
-    defaultLocale = "C.UTF-8"; #systemSettings.localeDefault;
+    defaultLocale = systemSettings.localeDefault;
     supportedLocales = [
-      "C.UTF-8"
-      # systemSettings.localeDefault
-      # systemSettings.localeSecondary
+      systemSettings.localeDefault
+      systemSettings.localeSecondary
     ];
     extraLocaleSettings = {
-      LC_ADDRESS = "C.UTF-8"; # systemSettings.localeDefault;
-      LC_IDENTIFICATION = "C.UTF-8"; # systemSettings.localeDefault;
-      LC_MEASUREMENT = "C.UTF-8"; # systemSettings.localeDefault;
-      LC_MONETARY = "C.UTF-8"; # systemSettings.localeDefault;
-      LC_NAME = "C.UTF-8"; # systemSettings.localeDefault;
-      LC_NUMERIC = "C.UTF-8"; # systemSettings.localeDefault;
-      LC_PAPER = "C.UTF-8"; # systemSettings.localeDefault;
-      LC_TELEPHONE = "C.UTF-8"; # systemSettings.localeDefault;
-      LC_TIME = "C.UTF-8"; # systemSettings.localeDefault;
+      LC_ADDRESS = systemSettings.localeDefault;
+      LC_IDENTIFICATION = systemSettings.localeDefault;
+      LC_MEASUREMENT = systemSettings.localeDefault;
+      LC_MONETARY = systemSettings.localeDefault;
+      LC_NAME = systemSettings.localeDefault;
+      LC_NUMERIC = systemSettings.localeDefault;
+      LC_PAPER = systemSettings.localeDefault;
+      LC_TELEPHONE = systemSettings.localeDefault;
+      LC_TIME = systemSettings.localeDefault;
     };
   };
 }
