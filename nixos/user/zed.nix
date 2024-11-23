96fda94b3565d16d4faa82a015359104fe66f332
JackMatanky <JackMatanky@gmail.com>
2024-11-23 12:07:17 +0200

    try adding simple-completion-language-server to zed.nix settings

--- nixos/user/zed.nix
+++ nixos/user/zed.nix
@@ -14,14 +14,19 @@
   snippetsDir = "${configDir}snippets/";
 in {
   home = {
-    packages = with pkgs-unstable; [
-      zed-editor
-      # inputs.simple-completion-language-server
-    ];
     file = {
       "${configDir}settings.json".source = "${zedDir}/settings.json";
       "${configDir}keymap.json".source = "${zedDir}/keymap.json";
       "${snippetsDir}".source = "${zedDir}/snippets";
     };
+    packages = with pkgs-unstable; [
+      zed-editor
+    ];
+    # ++ [
+    #   (pkgs.callPackage inputs.simple-completion-language-server {})
+    # ];
+  };
+  programs = {
+    simple-completion-language-server = pkgs.callPackage inputs.simple-completion-language-server {};
   };
 }
