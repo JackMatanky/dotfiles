0b13a09f2c132690d1bdd4199f62ffa037f94bea
MackMatanky <jackmatanky@gmail.com>
2024-12-04 12:34:53 +0200

    comment out IdentitiesOnly yes from ssh config

--- nixos/user/ssh.nix
+++ nixos/user/ssh.nix
@@ -7,21 +7,29 @@
 }: {
   programs.ssh = {
     enable = true;
+    addKeysToAgent = "confirm";
+    compression = false;
+    controlMaster = "no";
+    # controlPath = "~/.ssh/master-%r@%n:%p";
+    controlPersist = "no";
+    forwardAgent = false;
     hashKnownHosts = true;
+
+    # extraOptionOverrides = {};
     extraConfig = ''
       # GitHub SSH config
       Host github.com
           HostName github.com
           User git
           IdentityFile ~/.ssh/id_ed25519
-          IdentitiesOnly yes
+          # IdentitiesOnly yes
 
       # GitLab SSH config
       Host gitlab.com
           HostName gitlab.com
           User git
           IdentityFile ~/.ssh/id_ed25519
-          IdentitiesOnly yes
+          # IdentitiesOnly yes
     '';
   };
 }
