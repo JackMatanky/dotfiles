8a2d9606d93eb2c331ca4b4fc10a8b26d281085c
MackMatanky <jackmatanky@gmail.com>
2024-12-05 11:21:31 +0200

    update nix packages and home manager from 24.05 to 24.11

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,7 +2,7 @@
   description = "JackMatanky Flake Config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
 
     # nixos-hardware.url = "github:nixos/nixos-hardware";
@@ -12,7 +12,7 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
