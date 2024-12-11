5b84aea487bdb463b7eb9aa54e767e0813f18074
JackMatanky <JackMatanky@gmail.com>
2024-12-11 12:45:17 +0200

    fix espanso home manager config scoping

--- nixos/user/espanso.nix
+++ nixos/user/espanso.nix
@@ -16,113 +16,113 @@
       #   filter_title = "Visual Studio Code$";
       #   backend = "Clipboard";
       # };
-      matches = {
-        # Global Variables
-        global_vars = [
-          {
-            name = "my_first_name";
-            type = "echo";
+    };
+    matches = {
+      # Global Variables
+      global_vars = [
+        {
+          name = "my_first_name";
+          type = "echo";
+          params = {
+            echo = "Jack";
+          };
+        }
+        {
+          name = "my_last_name";
+          type = "echo";
+          params = {
+            echo = "Matanky";
+          };
+        }
+        {
+          name = "my_full_name";
+          type = "echo";
+          params = {
+            echo = "{{my_first_name}} {{my_last_name}}";
+          };
+        }
+        {
+          name = "my_email";
+          type = "echo";
+          params = {
+            echo = "{{my_first_name}}{{my_last_name}}@gmail.com";
+          };
+        }
+      ];
+      contact_vars = [
+        {
+          trigger = ":fullname";
+          replace = "{{my_full_name}}";
+        }
+        {
+          trigger = ":phone";
+          replace = "058-555-3200";
+        }
+        {
+          trigger = ":email";
+          replace = "{{my_email}}";
+        }
+        {
+          trigger = ":atb";
+          replace = "All the best,\n{{my_full_name}}";
+        }
+      ];
+      relative_dates = [
+        # Date
+        # Print today's date
+        {
+          trigger = ":today";
+          replace = "{{date_var}}";
+          vars = {
+            name = "date_var";
+            type = "date";
             params = {
-              echo = "Jack";
+              format = "%F";
             };
-          }
-          {
-            name = "my_last_name";
-            type = "echo";
+          };
+        }
+        {
+          trigger = ":tomorrow";
+          replace = "{{date_var}}";
+          label = "Insert tomorrow's date, such as 5-Jan-2022";
+          vars = {
+            name = "date_var";
+            type = "date";
             params = {
-              echo = "Matanky";
+              format = "%d.%m.%Y";
+              offset = 86400;
             };
-          }
-          {
-            name = "my_full_name";
-            type = "echo";
+          };
+        }
+        {
+          trigger = ":yesterday";
+          replace = "{{date_var}}";
+          label = "Insert yesterday's date, such as 5-Jan-2022";
+          vars = {
+            name = "date_var";
+            type = "date";
             params = {
-              echo = "{{my_first_name}} {{my_last_name}}";
+              format = "%d.%m.%Y";
+              offset = -86400;
             };
-          }
-          {
-            name = "my_email";
-            type = "echo";
+          };
+        }
+      ];
+      relative_times = [
+        # Time
+        # Print the current time
+        {
+          trigger = ":now";
+          replace = "{{time_var}}";
+          vars = {
+            name = "time_var";
+            type = "date";
             params = {
-              echo = "{{my_first_name}}{{my_last_name}}@gmail.com";
+              format = "%H:%M";
             };
-          }
-        ];
-        contact_vars = [
-          {
-            trigger = ":fullname";
-            replace = "{{my_full_name}}";
-          }
-          {
-            trigger = ":phone";
-            replace = "058-555-3200";
-          }
-          {
-            trigger = ":email";
-            replace = "{{my_email}}";
-          }
-          {
-            trigger = ":atb";
-            replace = "All the best,\n{{my_full_name}}";
-          }
-        ];
-        relative_dates = [
-          # Date
-          # Print today's date
-          {
-            trigger = ":today";
-            replace = "{{date_var}}";
-            vars = {
-              name = "date_var";
-              type = "date";
-              params = {
-                format = "%F";
-              };
-            };
-          }
-          {
-            trigger = ":tomorrow";
-            replace = "{{date_var}}";
-            label = "Insert tomorrow's date, such as 5-Jan-2022";
-            vars = {
-              name = "date_var";
-              type = "date";
-              params = {
-                format = "%d.%m.%Y";
-                offset = 86400;
-              };
-            };
-          }
-          {
-            trigger = ":yesterday";
-            replace = "{{date_var}}";
-            label = "Insert yesterday's date, such as 5-Jan-2022";
-            vars = {
-              name = "date_var";
-              type = "date";
-              params = {
-                format = "%d.%m.%Y";
-                offset = -86400;
-              };
-            };
-          }
-        ];
-        relative_times = [
-          # Time
-          # Print the current time
-          {
-            trigger = ":now";
-            replace = "{{time_var}}";
-            vars = {
-              name = "time_var";
-              type = "date";
-              params = {
-                format = "%H:%M";
-              };
-            };
-          }
-        ];
-      };
+          };
+        }
+      ];
     };
   };
 }
