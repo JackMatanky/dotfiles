f2dd44836e3708e648e16007ff18ae6df4695279
JackMatanky <JackMatanky@gmail.com>
2025-04-07 09:49:22 +0300

    move python data template to documents

--- _devtools/python/data_template/template_files/src/__init__.py
+++
@@ -1,6 +0,0 @@
-# -----------------------------------------------------------------------------
-# File: ${PROJECT_PATH}/src/__init__.py
-# Description: Makes src a Python module
-# -----------------------------------------------------------------------------
-
-# This file enables Python to treat the src directory as a package.
