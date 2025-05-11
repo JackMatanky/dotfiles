b6520daed41df712591977df8daa1f0cf96e6f5a
JackMatanky <JackMatanky@gmail.com>
2025-05-11 22:41:26 +0300

    try setting catppuccin to transparent

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -16,6 +16,12 @@ return {
     name = "catppuccin",
     opts = {
       flavour = "macchiato",
+      transparent_background = true,
+      dim_inactive = {
+        enabled = false, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+      },
       integrations = {
         aerial = true,
         alpha = true,
@@ -35,11 +41,20 @@ return {
         mini = true,
         native_lsp = {
           enabled = true,
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+          },
           underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
+            errors = { "underline" },
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
+          },
+          inlay_hints = {
+            background = true,
           },
         },
         navic = { enabled = true, custom_bg = "lualine" },
